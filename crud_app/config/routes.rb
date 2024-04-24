d6e0f15772db47ec3a2aeae5beb05b0d0025a4b6
Spencer Lewis <spence.914@gmail.com>
2024-04-24 15:47:56 -0700

    updatre routes, define views, seed db

--- crud_app/config/routes.rb
+++ crud_app/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+  root 'programming_languages#index'
   resources :programming_languages
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
--- crud_app/db/seeds.rb
+++ crud_app/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ProgrammingLanguage.create([
+  { title: 'Ruby', description: 'A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.', link: 'https://www.ruby-lang.org/' },
+  { title: 'Python', description: 'Python is a programming language that lets you work quickly and integrate systems more effectively.', link: 'https://www.python.org/' },
+  { title: 'JavaScript', description: 'JavaScript is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions.', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+  { title: 'Go', description: 'Go is an open source programming language that makes it easy to build simple, reliable, and efficient software.', link: 'https://golang.org/' }
+])
